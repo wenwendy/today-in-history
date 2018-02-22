b98763c993a0f42d4ac4991f1c99d570cd3f46c9
Wendy Wang <wendyw@pageuppeople.com>
2018-02-22 15:51:05 +1100

    Functional components way

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 
-class Card extends Component  {
 
-  render() {
-    return (
-      <div>
-        <div class="title">{this.props.title}</div>
-        <div class="name">{this.props.name}</div>
-        <div class="description">{this.props.description}</div>
-      </div>
-    );
-  }
+function Card(props) {
+  return (
+    <div>
+      <div class="title">{props.title}</div>
+      <div class="name">{props.name}</div>
+      <div class="description">{props.description}</div>
+    </div>
+  );
 }
 
 export default Card;
