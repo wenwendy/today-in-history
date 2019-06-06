ac8ff699610cbd40411e18aa9c7b5fe1e2ca3ea0
Wendy Wang <wendyw@pageuppeople.com>
2019-06-06 16:07:33 +1000

    Updates

---
+++ precache-manifest.da85282a9d867cc489d130dab41acb8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae33c19b3e019a70ff2798ee6fde0216",
+    "url": "/today-in-history/index.html"
+  },
+  {
+    "revision": "21cb2db4b347e11e0583",
+    "url": "/today-in-history/static/css/main.2e4f30fb.chunk.css"
+  },
+  {
+    "revision": "edb20e94b290a94bca74",
+    "url": "/today-in-history/static/js/2.33ffb0e7.chunk.js"
+  },
+  {
+    "revision": "21cb2db4b347e11e0583",
+    "url": "/today-in-history/static/js/main.114bd9bc.chunk.js"
+  },
+  {
+    "revision": "a6370a7080a2199a5456",
+    "url": "/today-in-history/static/js/runtime~main.527d0d92.js"
+  },
+  {
+    "revision": "6695892a07628ce5a2de12176c90e1ab",
+    "url": "/today-in-history/static/media/matthew-kane-278419-unsplash.6695892a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/today-in-history/precache-manifest.d9b34754d3bca9288ca3a8942d3f15a2.js"
+  "/today-in-history/precache-manifest.da85282a9d867cc489d130dab41acb8f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/today-in-history/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/today-in-history/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
